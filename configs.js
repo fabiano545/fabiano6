e3bf0418c36c9abfb49df2bab217d3645ddc82f2
fabiano545 <64483145+fabiano545@users.noreply.github.com>
2020-09-29 10:12:52 +0200

    Update configs.js

--- configs.js
+++ configs.js
@@ -1,5 +1,4 @@
 module['exports'] = {
-  TOKEN: 'mfa.rStLQPPaxVORvk0WodRSVoV2c8nZi4BSx_E1zIoE9vmdtYDUcaA0SWHDc2H9ssJeCeMKr5NnqSP
-HkuExqmeE',
+  TOKEN: 'mfa.rStLQPPaxVORvk0WodRSVoV2c8nZi4BSx_E1zIoE9vmdtYDUcaA0SWHDc2H9ssJeCeMKr5NnqSPHkuExqmeE',
   PREFIX: '!'
 };
